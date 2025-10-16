f5dcb4e68f711ca0a34d393551aeba7e62639044
wiredan <mtlhumaid@gmail.com>
2025-10-16 12:45:47 -0700

    Update main.jsx

--- DAN-NETWORK-main/frontend/src/main.jsx
+++ DAN-NETWORK-main/frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+// StrictMode is good for catching dev warnings
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
