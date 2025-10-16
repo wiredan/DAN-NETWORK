af72bab0303a75933e344964d8fa65a5540be1b5
wiredan <mtlhumaid@gmail.com>
2025-10-16 10:50:04 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 🛠️ FIX: Tells Vite to use the 'src' directory as the project root, 
-  // where it expects to find index.html
-  root: 'src', 
+  root: 'frontend', // use the folder with index.html
+  build: {
+    outDir: '../dist',
+  },
   server: {
     port: 5173,
-    proxy: {
-      // during local dev, forward API calls to worker dev server
-      "/api": {
-        target: "http://127.0.0.1:8787",
-        changeOrigin: true
-      }
-    }
   },
-  build: {
-    outDir: "dist"
-  }
-});
+})
