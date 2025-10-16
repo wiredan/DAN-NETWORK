74e46ec6582f040f48c771e82cdedafc26f8d0cf
wiredan <mtlhumaid@gmail.com>
2025-10-16 11:31:24 -0700

    Create DAN-NETWORK-main/frontend/vite.config.js

---
+++ DAN-NETWORK-main/frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "../dist", // output folder at project root
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
